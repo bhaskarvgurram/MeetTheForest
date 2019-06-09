6cfa2eceb57fe674deb01e5fd3512d11a1e681df
ultimatehrishi <hrishikeshwaikar@gmail.com>
2019-06-09 12:01:01 -0700

    Questions

--- screens/tracks.js
+++ screens/tracks.js
@@ -6,39 +6,30 @@ export var track = [{
     alerts: ["Beware of the bears coming to drink water off the river"],
     info_messages: ["Sentinel Beach is on the Merced River", "Summer is the best time for getting into water"],
     game_messages: [{
-        question: "How many water falls do you see from the bridge?",
+        question: "How many water falls do you see from the beach?",
+        option1: "4",
+        option2: "1",
+        option3: "2",
         answer: "2"
-    }, {
-        question: "Which direction does the water of the river flow?",
-        answer: "South",
     }],
-    docusign_messages: [{
-        title: "Save me",
-        message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-    }, {
-        title: "Roadless Area Protection",
-        message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
-    }]
+    docusign_messages: []
 
 }, {
     latitude: "35.7908536",
     longitude: "-120.3967217",
     completed: false,
-    alerts: ["Cool winds are nice", "Beware of the dogs"],
+    alerts: [],
     info_messages: ["The Yosemite Creek Bridge is the oldest, carrying the North Road and spanning Yosemite Creek below Yosemite Falls", "Creek Bridge was built in 1922, it spans 50 feet (15 m) in a single arch of reinforced concrete faced with granite."],
     game_messages: [{
-        question: "How many water falls do you see from the bridge?",
-        answer: "2"
-    }, {
-        question: "Which direction does the water of the river flow?",
-        answer: "South",
+        question: "Somewhere on the bridge, the name of the president is written who loved visiting this place?",
+        option1: "Roosevelt",
+        optionB: "Lyndon Johnson",
+        optionC: "JFK",
+        answer: "Roosevelt"
     }],
     docusign_messages: [{
         title: "Save me",
         message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-    }, {
-        title: "Roadless Area Protection",
-        message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
     }]
 
 }
@@ -54,19 +45,13 @@ export var visitingSpots = [
         alerts: ["Official policy does not allow visitors to jump off the bridge"],
         info_messages: ["Sentinel Beach is on the Merced River", "Summer is the best time for getting into water"],
         game_messages: [{
-            question: "How many water falls do you see from the bridge?",
-            answer: "2"
-        }, {
             question: "Which direction does the water of the river flow?",
+            option1: "North",
+            option2: "South",
+            option3: "West",
             answer: "South",
         }],
-        docusign_messages: [{
-            title: "Save me",
-            message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-        }, {
-            title: "Roadless Area Protection",
-            message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
-        }]
+        docusign_messages: []
 
     },
     {
@@ -84,11 +69,8 @@ export var visitingSpots = [
             answer: "South",
         }],
         docusign_messages: [{
-            title: "Save me",
-            message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-        }, {
             title: "Roadless Area Protection",
-            message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
+            message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can help me further by signing this petition."
         }]
 
     },
@@ -100,19 +82,13 @@ export var visitingSpots = [
         alerts: ["Waterfalls are beautiful", "Beware of the bears"],
         info_messages: ["The chapel, which is the oldest structure in Yosemite Valley, was designed by Charles Geddes", "Designed in a 'New England style' to seat 250 people"],
         game_messages: [{
-            question: "How many water falls do you see from the bridge?",
-            answer: "2"
-        }, {
-            question: "Which direction does the water of the river flow?",
-            answer: "South",
+            question: "How many apple trees can you find in the componund of the Chapel",
+            option1: "3",
+            option2: "10",
+            option3: "5",
+            answer: "5"
         }],
-        docusign_messages: [{
-            title: "Save me",
-            message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-        }, {
-            title: "Roadless Area Protection",
-            message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
-        }]
+        docusign_messages: []
 
     },
     {
@@ -123,19 +99,13 @@ export var visitingSpots = [
         alerts: ["Waterfalls are beautiful", "Beware of the bears"],
         info_messages: ["Yosemite Conservation Heritage Center was built in 1974 to connect the east yosemite to west yosemite"],
         game_messages: [{
-            question: "How many water falls do you see from the bridge?",
+            question: "W?",
             answer: "2"
         }, {
             question: "Which direction does the water of the river flow?",
             answer: "South",
         }],
-        docusign_messages: [{
-            title: "Save me",
-            message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-        }, {
-            title: "Roadless Area Protection",
-            message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
-        }]
+        docusign_messages: []
 
     },
     {
@@ -152,13 +122,7 @@ export var visitingSpots = [
             question: "Which direction does the water of the river flow?",
             answer: "South",
         }],
-        docusign_messages: [{
-            title: "Save me",
-            message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-        }, {
-            title: "Roadless Area Protection",
-            message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
-        }]
+        docusign_messages: []
 
     },
     {
@@ -175,13 +139,7 @@ export var visitingSpots = [
             question: "Which direction does the water of the river flow?",
             answer: "South",
         }],
-        docusign_messages: [{
-            title: "Save me",
-            message: "Would you like to save me from the deforestration and senseless mining, drilling and land clearing activities. My health is deteriorating. Please sign this petition to save me."
-        }, {
-            title: "Roadless Area Protection",
-            message: "Vast regions of my lands are under threat from the subversion of the Roadless Areas Protection Act by the current law makers. You can save me by signing this petition."
-        }]
+        docusign_messages: []
 
     },
 ]
