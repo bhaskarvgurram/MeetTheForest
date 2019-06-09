1c52f4c41f145f1f25ad1bfdd39985ed3dcd6773
tanajijadhav <tanajigjadhav@gmail.com>
2019-06-09 00:26:23 -0700

    Errors resolved

--- components/Map.js
+++ components/Map.js
@@ -10,7 +10,7 @@ import MapViewDirections from 'react-native-maps-directions';
 
 export default class googleMap extends Component {
     state = {
-        nearestVicinityLimit = 50 // set by the owner
+        nearestVicinityLimit = 50, // set by the owner
         myLocation: {},
         errorMessage: null,
         visitingSpots : [
@@ -102,11 +102,11 @@ export default class googleMap extends Component {
 
     }
 
-    var rad = function(x) {
+    rad = function(x) {
         return x * Math.PI / 180;
-      };
+    };
       
-      var getDistance = function(p1, p2) {
+    getDistance = function(p1, p2) {
         var R = 6378137; // Earth’s mean radius in meter
         var dLat = rad(p2.lat() - p1.lat());
         var dLong = rad(p2.lng() - p1.lng());
